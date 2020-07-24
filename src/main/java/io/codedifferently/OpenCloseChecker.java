952655b6af769ede1df5d878933d2e6ba46cfcea
esanvee <eesanvee18@students.desu.edu>
2020-07-24 12:33:46 -0400

    Done

--- src/main/java/io/codedifferently/OpenCloseChecker.java
+++ src/main/java/io/codedifferently/OpenCloseChecker.java
@@ -1,4 +1,74 @@
 package io.codedifferently;
 
+import java.util.Stack;
+
 public class OpenCloseChecker {
+
+    public static void main(String args[]) {
+
+		String testString = ")))";
+		System.out.println(isMatching(testString));
+	}
+
+	public static boolean isMatching(String str) {
+
+		Stack<Character> buffer = new Stack<Character>();
+		//buffer must be empty to be true (No lonely/extra brackets)
+
+		for (int i = 0; i < str.length(); i++) {
+
+            
+
+            // If I find the beginning
+
+			if (str.charAt(i) == '(' || str.charAt(i) == '[' || str.charAt(i) == '{' || str.charAt(i) == '<' || str.charAt(i) == '"') {
+                
+                buffer.push(str.charAt(i)); //add to the stack
+                
+
+			}
+			
+			// if I find the end
+			if (str.charAt(i) == ')' || str.charAt(i) == ']' || str.charAt(i) == '}' || str.charAt(i) == '>' || str.charAt(i) == '"') {
+				
+				
+				if (buffer.empty()) {
+					return false;
+				}
+				
+                    //If there's a match, delete it from the stack
+                    
+				else if (buffer.peek() == '(' && str.charAt(i) == ')') {
+					buffer.pop();
+				}
+				else if (buffer.peek() == '[' && str.charAt(i) == ']') {
+					buffer.pop();
+				}
+				else if (buffer.peek() == '{' && str.charAt(i) == '}') {
+					buffer.pop();
+                }
+                else if (buffer.peek() == '<' && str.charAt(i) == '>') {
+                    buffer.pop();
+                    
+                }
+                
+                else if (buffer.peek() == '"' && str.charAt(i) == '"') {
+                    buffer.pop();
+                    
+                }
+                
+                
+
+            }
+            
+            
+
+			
+        }
+
+
+		return buffer.isEmpty();
+
+	}
+
 }
--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,61 @@
 package io.codedifferently;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class OpenCloseCheckerTest {
+
+    @Test
+
+    public void testOpenClose(){
+
+        String test = "a(b)c(d)e(f)g)";
+
+        boolean expected = true;
+        boolean actual = OpenCloseChecker.isMatching(test);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void testOpenClose2(){
+
+        String test = ")";
+
+        boolean expected = false;
+        boolean actual = OpenCloseChecker.isMatching(test);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void testOpenClose3(){
+
+        String test = "a(bcd(ef))";
+
+        boolean expected = true;
+        boolean actual = OpenCloseChecker.isMatching(test);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void testOpenClose4(){
+
+        String test = "a(bc(d)))";
+
+        boolean expected = false;
+        boolean actual = OpenCloseChecker.isMatching(test);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
 
 }
